66162c9321e9ff5fabf0cec97b9e5df2acb2eec3
t.topchu <ttopchu@tu-sofia.bg>
2017-05-17 17:23:46 +0300

    Add files via upload

---
+++ nrf24l01_receiver.ino
@@ -0,0 +1,39 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+RF24 radio(7,8);
+int cmd[100];
+int LED2 = 3;
+int LED1 = 4;
+int LED3 = 5;
+
+const uint64_t pipe = 0xDEDEDEDEE7LL;
+
+void setup(void){
+ Serial.begin(115200);
+ radio.begin();
+ radio.setChannel(108); 
+ radio.setPALevel(RF24_PA_MAX);
+ radio.setDataRate(RF24_250KBPS);
+ radio.openReadingPipe(1,pipe);
+ radio.startListening();
+ pinMode(LED1, OUTPUT);
+ pinMode(LED2,OUTPUT);
+ pinMode(LED3,OUTPUT);
+}
+void loop(void){
+ if (radio.available()){
+  digitalWrite(LED1,HIGH);
+ // digitalWrite(LED3, LOW);
+   while (radio.available()){
+     radio.read(&cmd,sizeof(cmd));      
+     Serial.println(cmd[0]);
+     if (cmd[0] == 'A')digitalWrite(LED2, HIGH);
+     else {digitalWrite(LED2, LOW);}
+      }
+ }
+ else{Serial.println("No radio available");
+       digitalWrite(LED1, LOW);
+      // digitalWrite(LED3, HIGH);
+      }}
---
+++ nrf24l01_transmitter.ino
@@ -0,0 +1,30 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+RF24 radio(7,8);
+int cmd[100];
+int SW1 = 4;
+int led1=2;
+
+const uint64_t pipe = 0xDEDEDEDEE7LL;
+
+void setup(void){
+ Serial.begin(115200);
+ radio.begin();
+ radio.setChannel(108);
+ radio.setPALevel(RF24_PA_MIN);
+ radio.setDataRate(RF24_250KBPS);
+ radio.openWritingPipe(pipe);
+ pinMode(led1,OUTPUT);
+ pinMode(3,OUTPUT);
+ digitalWrite(led1,LOW);
+}
+
+void loop(void){
+ digitalWrite(3,HIGH);
+ int pok=digitalRead(SW1);
+ if(pok==HIGH)cmd[0]='A';
+ else cmd[0]=0;
+ radio.write(&cmd,sizeof(cmd));
+}
